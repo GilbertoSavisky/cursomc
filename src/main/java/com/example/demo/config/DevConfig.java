fb1aefc360080b92c9b9a2ea67b506f54961417e
Gilberto Savisky <saviskydeveloper@gmail.com>
2019-02-07 15:47:16 -0200

    Criando o profile de desenvolvimento

--- src/main/java/com/example/demo/config/DevConfig.java
+++ src/main/java/com/example/demo/config/DevConfig.java
@@ -1,20 +1,34 @@
 package com.example.demo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.demo.services.DBService;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class DevConfig {
 	
-	//@Autowired
-	//private DBService dbService;
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
 	
 	@Bean
-	public boolean instantiateDatabase() {
+	public boolean instantiateDatabase() throws ParseException {
 		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
 		return true;
 	}
 
